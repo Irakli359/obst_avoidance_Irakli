0e9fd47facef78683db7fc4692545afde0a325cf
irakli359 <ikelb14@freeuni.edu.ge>
2019-05-24 17:47:13 +0200

    1st commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/irakli/mybot_ws/src
+# Build directory: /home/irakli/mybot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(mybot_control)
+subdirs(mybot_description)
+subdirs(mybot_gazebo)
+subdirs(teleop_twist_keyboard)
+subdirs(icontrol)
+subdirs(motion_plan)
---
+++ build/icontrol/CMakeFiles/setvel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/setvel.dir/src/setvel.cpp.o"
+  "/home/irakli/mybot_ws/devel/lib/icontrol/setvel.pdb"
+  "/home/irakli/mybot_ws/devel/lib/icontrol/setvel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/setvel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
